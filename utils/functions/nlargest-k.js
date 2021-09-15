f12371f9728079817dd1dc33d2a64090bd1165d9
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-09-14 20:56:31 -0700

    nlargest k algo with test

---
+++ utils/functions/nlargest-k.js
@@ -0,0 +1,17 @@
+module.exports = function(values, n, k) {
+  // keep only multiples of k
+  // make sure to keep only unique
+  let filteredValues = values.filter(x => { return x % k === 0 });
+  filteredValues.sort((x, y) => { return x - y });
+  filteredValues = [ ...new Set(filteredValues) ];
+
+
+  // edge cases, just return 0
+  if (values.length < n) return -1;
+  if (filteredValues < n) return -1;
+  if (filteredValues.length - n < 0) return -1;
+
+  // return the nth largest from the array if it exists
+  return filteredValues[filteredValues.length - n];
+
+}
