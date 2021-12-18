009d451d4cc01b031fc79faab8e9bd347190d149
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-12-17 21:09:01 -0800

    remove old tests

--- utils/functions/fibonnaci-index.js
+++
@@ -1,9 +0,0 @@
-function fibonnaci(index) {
-  if (index === 0) return 0;
-  if (index === 1) return 1;
-  if (index === 2) return 1;
-
-  return fibonnaci(index -1) + fibonnaci(index - 2);
-}
-
-module.exports = fibonnaci;
