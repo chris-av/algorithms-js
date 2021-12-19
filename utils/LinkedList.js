24915135b1b418004eb2fd4f472ddf9ffd686b46
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-12-18 23:23:37 -0800

    wrap in parenthesis just in case

--- utils/LinkedList.js
+++ utils/LinkedList.js
@@ -1,7 +1,7 @@
 class LinkedList {
   constructor(data, next) {
     this.data = data;
-    this.next = next === undefined ? null : next;
+    this.next = (next === undefined ? null : next);
   }
 }
 
