b8a6c0c5c0bc7a0a28947bc217d0d3a602bd6d06
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-12-22 22:28:16 -0800

    add rotate-image algo

---
+++ src/rotate-image/index.test.js
@@ -0,0 +1,25 @@
+const rotateImage = require('./');
+
+describe('rotate image', () => {
+
+  let testarr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+
+  test('rotate left', () => {
+    const testResult = [ [ 3, 6, 9 ], [ 2, 5, 8 ], [ 1, 4, 7 ] ];
+    const result = rotateImage(testarr, 'left');
+    expect(result).toEqual(testResult);
+  });
+
+  test('rotate right', () => {
+    const testResult = [ [ 7, 4, 1 ], [ 8, 5, 2 ], [ 9, 6, 3 ] ];
+    const result = rotateImage(testarr, 'right');
+    expect(result).toEqual(testResult);
+  });
+
+
+});
+
