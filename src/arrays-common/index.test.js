66df1ba4f0005b45e1792fa56154de17af88e66d
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-10-23 00:54:15 -0700

    finding common array within array of arrays

---
+++ src/arrays-common/index.test.js
@@ -0,0 +1,40 @@
+const commonArr = require('./');
+
+
+describe('get common array', () => {
+
+  test('test 1', () => {
+    const arrayOfArrays = [
+      [ 5, 10, 15, 20 ],
+      [ 15, 88, 1, 5, 7 ],
+      [ 1, 10, 15, 5, 20 ],
+      [ 5, 15 ],
+    ];
+    const testResult = [ 5, 15 ];
+    const result = commonArr(arrayOfArrays);
+    expect(result).toEqual(testResult);
+  });
+
+  test('test 2', () => {
+    const arrayOfArrays = [
+      [ 5, 10, 15, 20 ],
+      [ 15, 88, 1, 5, 7, 20 ],
+      [ 1, 10, 15, 5, 20 ],
+      [ 5, 15, 20 ],
+    ];
+    const testResult = [ 5, 15, 20 ];
+    const result = commonArr(arrayOfArrays);
+    expect(result).toEqual(testResult);
+  });
+
+  test('test 3', () => {
+    const arrayOfArrays = [
+      [ 1, 2, 3 ],
+      [ 2, 3 ],
+    ];
+    const testResult = [ 2, 3 ];
+    const result = commonArr(arrayOfArrays);
+    expect(result).toEqual(testResult);
+  });
+
+})
