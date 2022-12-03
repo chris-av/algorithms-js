7f9c2aa6dafbcc31457c8c297da02fee098b0085
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-12-02 18:42:27 -0800

    algo for efficienty doing things on first pass
    This algorithm I did initially in On2. Friend explains that brute force
    is viable but could be better, and explained a first pass solution.
    
    Trick is to store only the values that you need. Take advantage of the
    fact that stock profit can only be derived by selling stock today in the
    future. Not vice versa.

---
+++ index.js
@@ -0,0 +1,13 @@
+const findMaxProfit = require('./src/stock-profit');
+
+
+const stocks = [
+  1000, 10, 10, 30, 50
+];
+
+
+const result = findMaxProfit(stocks);
+console.log(result)
+
+
+
---
+++ src/stock-profit/index.js
@@ -0,0 +1,16 @@
+// given an array of prices for a stock, calculate the max profit
+// where you have to purchase a stock in one day, and sell it another day
+module.exports = function(stocks) {
+  let maxProfit = 0;
+  let lowestFound = stocks[0];
+
+  for (let i = 0; i < stocks.length; i++) {
+    if (stocks[i] < lowestFound) { lowestFound = stocks[i]; }
+    let diff = stocks[i] - lowestFound;
+    if (diff > maxProfit) { maxProfit = diff; }
+  }
+
+  return maxProfit < 0 ? 0 : maxProfit;
+
+}
+
