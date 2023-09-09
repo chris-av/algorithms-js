fa3cb21a65c59b7a4fb46f8021b7c1161f6f6194
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-09-09 15:12:25 -0700

    migrate to ts config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const config = {
-  verbose: true,
-  moduleNameMapper: {
-    "^@utils(.*)$": "<rootDir>/utils$1"
-  }
-}
-
-module.exports = config;
---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json'
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+
+const config: JestConfigWithTsJest = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  roots: ["<rootDir>"],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+}
+
+export default config;
+
